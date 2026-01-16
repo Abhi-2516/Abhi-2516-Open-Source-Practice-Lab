29f9f5a272119d59d3e22adb2a712ab811d3e142
Abdul Wahab <114802883+abdul-wahab619@users.noreply.github.com>
2026-01-16 12:41:14 +0500

    Implement word count functionality in count-words.py

--- tasks/python/easy/count-words.py
+++ tasks/python/easy/count-words.py
@@ -9,3 +9,10 @@ Output:
 """
 
 # TODO: Write your solution here
+
+sentence = "open source is awesome"
+
+# Split the sentence into words and count them
+word_count = len(sentence.split())
+
+print(word_count)
