549467c81ba009a07ea11e4eed8207921bb6117f
Nikitha <dnikitha@yahoo.in>
2026-05-29 18:19:34 +0530

    Add: find maximum python task

---
+++ tasks/python/easy/find-maximum.py
@@ -0,0 +1,14 @@
+"""
+TASK: Find Maximum Number
+
+Requirements:
+- Write a function that returns the largest number in a list.
+
+Example:
+Input: [4, 2, 9, 1, 7]
+Output: 9
+"""
+
+def find_maximum(numbers):
+    # TODO: Implement function
+    pass
